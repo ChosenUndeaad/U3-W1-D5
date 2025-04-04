5398257f8797841015c12b13dd35469729489474
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-04 11:50:56 +0200

    Update

--- u2w1d5/src/App.jsx
+++ u2w1d5/src/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import StarWarsRow from "./Components/StarWarsRow";
+import LotrMovies from "./Components/LotrRow";
+import ScreamMovies from "../../ScreamRow";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <div className='bg-dark text-light min-vh-100'>
+        <Navbar />
+        <StarWarsRow />
+        <LotrMovies />
+        <ScreamMovies />
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ u2w1d5/src/Components/LotrRow.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class LotrMovies extends Component {
+  state = {
+    movies: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=8a9ae283&s=Lord-Of-The-Rings")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          movies: data.Search,
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.error("Errore nel recupero dati:", error);
+        this.setState({ loading: false });
+      });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className='container-fluid mx-2 mt-4'>
+        <div className='row my-2'>
+          <h2>Rewatch Later: Lord of the Rings</h2>
+        </div>
+        <div className='row g-2 m-2'>
+          {movies.slice(0, 6).map((movie) => (
+            <div key={movie.imdbID} className='col-12 col-md-4 col-lg-2 p-1'>
+              <img src={movie.Poster} alt={movie.Title} className='w-100' />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LotrMovies;
