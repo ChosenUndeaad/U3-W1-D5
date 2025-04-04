469f4bf70cb4673be35f483570b3fb2a7e5d4ab6
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-04 14:44:41 +0200

    Updated navbar

--- u2w1d5/src/Components/Navbar.jsx
+++ u2w1d5/src/Components/Navbar.jsx
@@ -1,82 +1,53 @@
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-function Navbar() {
+function MyNavbar() {
   return (
-    <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <div class='container-fluid'>
-        <a class='navbar-brand' href='#'>
-          <img src='../assets/netflix_logo.png' alt='Netflix' width='100' />
-        </a>
-        <button
-          class='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNav'
-          aria-controls='navbarNav'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span class='navbar-toggler-icon'></span>
-        </button>
-        <div class='collapse navbar-collapse' id='navbarNav'>
-          <ul class='navbar-nav'>
-            <li class='nav-item'>
-              <a class='nav-link active' aria-current='page' href='#'>
-                Home
-              </a>
-            </li>
-            <li class='nav-item fw-bold'>
-              <a class='nav-link active' aria-current='page' href='#'>
-                TV Shows
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link active' aria-current='page' href='#'>
-                Movies
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link active' aria-current='page' href='#'>
-                Recently Added
-              </a>
-            </li>
-            <li class='nav-item'>
-              <a class='nav-link active' aria-current='page' href='#'>
-                My List
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class='dropdown'>
-        <button
-          class='btn btn-dark dropdown-toggle'
-          type='button'
-          data-bs-toggle='dropdown'
-          aria-expanded='false'
-        >
-          <img src='./Netflix-assets/assets/avatar.png' width='30' />
-        </button>
-        <ul class='dropdown-menu dropdown-menu-end bg-dark'>
-          <li>
-            <a class='dropdown-item text-light' href='./user-page.html'>
-              Edit Profile
-            </a>
-          </li>
-          <li>
-            <a class='dropdown-item text-light' href='#'>
-              Settings
-            </a>
-          </li>
-          <li>
-            <a class='dropdown-item text-light' href='#'>
-              Switch User
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar expand='lg' className='bg-dark text-light' data-bs-theme='dark'>
+      <Container fluid>
+        <Navbar.Brand href='#home'>
+          <img
+            src='../assets/netflix_logo.png'
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+            alt='React Bootstrap logo'
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#tvshows'>TV Shows</Nav.Link>
+            <Nav.Link href='#movies' className='fw-bold'>
+              Movies
+            </Nav.Link>
+            <Nav.Link href='#recently-added'>Recently Added</Nav.Link>
+            <Nav.Link href='#my-list'>My List</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+        <Navbar.Collapse className='justify-content-end'>
+          <Dropdown>
+            <Dropdown.Toggle
+              id='dropdown-basic'
+              className='text-light bg-dark border-dark'
+            >
+              User
+            </Dropdown.Toggle>
+            <Dropdown.Menu className='dropdown-menu-end'>
+              <Dropdown.Item href='#action-1'>Edit profile</Dropdown.Item>
+              <Dropdown.Item href='#action-2'>Settings</Dropdown.Item>
+              <Dropdown.Item href='#action-3'>Switch User</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default MyNavbar;
--- u2w1d5/src/UserPage.jsx
+++ u2w1d5/src/UserPage.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import StarWarsRow from "./Components/StarWarsRow";
-import LotrMovies from "./Components/LotrRow";
-import ScreamMovies from "./Components/ScreamRow";
 
 function App() {
   return (
     <>
       <div className='bg-dark text-light min-vh-100'>
         <MyNavbar />
-        <StarWarsRow />
-        <LotrMovies />
-        <ScreamMovies />
+
         <Footer />
       </div>
     </>
