91a744d66d098a7a735eff60ad640e7535cfcdfa
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-09 16:04:09 +0200

    Update TvShowDetails.jsx

--- u2w1d5/src/Components/TvShowDetails.jsx
+++ u2w1d5/src/Components/TvShowDetails.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import React, { useState, useEffect } from "react";
 
 function TvShowDetail() {
+  const movieData = (movieId) => {
+    const [movie, setMovie] = useState(null);
+    const apiKey = "8a9ae283";
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `http://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}`
+          );
+          const data = await response.json();
+          setMovie(data);
+        } catch (error) {
+          console.error("Errore nel recupero dei dati del film:", error);
+        }
+      };
+
+      fetchData();
+    }, [movieId, apiKey]);
+    return movie;
+  };
+
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant='top' src='holder.js/100px180' />
+      <Card.Img variant='top' src={movie.Poster} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Plot}</Card.Text>
+        <Button variant='primary'>More info</Button>
       </Card.Body>
     </Card>
   );
 }
-
 export default TvShowDetail;
